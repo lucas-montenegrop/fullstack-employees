db1424a4c4cd8b3aa97e40a039915604e5ade74a
Lucas Montenegro <lmp.montenegro@gmail.com>
2026-03-04 14:57:35 -0500

    is it done not sure what the final version of these repo should look like

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
+CREATE DATABASE IF NOT EXISTS fullstack_employees;
+
 DROP TABLE IF EXISTS employees;
 
-
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  birthday DATE,
+  salary INTEGER
+);
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,24 @@
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
 
+const employees = [
+  { name: "Test 1", birthday: new Date("1990-01-01"), salary: 75000 },
+  { name: "Test 2", birthday: new Date("1992-02-02"), salary: 80000 },
+  { name: "Test 3", birthday: new Date("1994-03-03"), salary: 90000 },
+];
+
+async function seedEmployee(employee) {
+  return await createEmployee(employee);
+}
+
+async function seedEmployees() {
+  for (const employee of employees) {
+    await seedEmployee(employee);
+  }
+}
+
+// ✅ run AFTER everything is defined
 await db.connect();
 await seedEmployees();
 await db.end();
 console.log("🌱 Database seeded.");
-
-async function seedEmployees() {
-  // TODO
-}
